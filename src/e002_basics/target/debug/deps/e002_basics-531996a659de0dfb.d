a1147819f54eafcf4e082afea5380243b7d19765
Mert <mertturkmenoglu99@gmail.com>
2019-07-18 01:18:55 +0300

    Add language basics example

---
+++ src/e002_basics/target/debug/deps/e002_basics-531996a659de0dfb.d
@@ -0,0 +1,5 @@
+/home/mert/codes/rust/src/e002_basics/target/debug/deps/e002_basics-531996a659de0dfb: src/main.rs
+
+/home/mert/codes/rust/src/e002_basics/target/debug/deps/e002_basics-531996a659de0dfb.d: src/main.rs
+
+src/main.rs:
---
+++ src/e002_basics/target/debug/e002_basics.d
@@ -0,0 +1,1 @@
+/home/mert/codes/rust/src/e002_basics/target/debug/e002_basics: /home/mert/codes/rust/src/e002_basics/src/main.rs
