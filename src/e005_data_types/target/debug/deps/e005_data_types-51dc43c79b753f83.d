776891b28c624388bc1e4d0eec2124ba3dd21995
Mert <mertturkmenoglu99@gmail.com>
2019-07-18 01:27:12 +0300

    Add data types example

---
+++ src/e005_data_types/target/debug/deps/e005_data_types-51dc43c79b753f83.d
@@ -0,0 +1,5 @@
+/home/mert/codes/rust/src/e005_data_types/target/debug/deps/e005_data_types-51dc43c79b753f83: src/main.rs
+
+/home/mert/codes/rust/src/e005_data_types/target/debug/deps/e005_data_types-51dc43c79b753f83.d: src/main.rs
+
+src/main.rs:
---
+++ src/e005_data_types/target/debug/e005_data_types.d
@@ -0,0 +1,1 @@
+/home/mert/codes/rust/src/e005_data_types/target/debug/e005_data_types: /home/mert/codes/rust/src/e005_data_types/src/main.rs
