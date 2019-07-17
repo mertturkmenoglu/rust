19375f09f3b655cbc99c91935cdcc28cc6924ccc
Mert <mertturkmenoglu99@gmail.com>
2019-07-18 01:10:42 +0300

    Initial commit

---
+++ src/e001_hello_world/target/debug/deps/e001_hello_world-f5a94d2e1be25203.d
@@ -0,0 +1,5 @@
+/home/mert/codes/rust/src/e001_hello_world/target/debug/deps/e001_hello_world-f5a94d2e1be25203: src/main.rs
+
+/home/mert/codes/rust/src/e001_hello_world/target/debug/deps/e001_hello_world-f5a94d2e1be25203.d: src/main.rs
+
+src/main.rs:
